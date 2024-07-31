3b49e3d89052bf91af34a728daaf2af16c6f310d
customer-manager <cmanager747@gmail.com>
2024-07-31 16:20:02 +0300

    added_solves

---
+++ src/store/store.tsx
@@ -0,0 +1,15 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import authReducer from './reducers/AuthReducer';
+
+const rootReducer = combineReducers({
+  auth:authReducer
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
