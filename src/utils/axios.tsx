9fc49f6200e0db54f870b45fb8d95e0511ccd6a2
customer-manager <cmanager747@gmail.com>
2024-08-19 00:02:42 +0300

    changed database

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const BASE_URL="https://server-n6lp.onrender.com";
+export const BASE_URL="https://postgresql-server.onrender.com";
 
-const axiosServices = axios.create({ baseURL: 'https://server-n6lp.onrender.com' });
+const axiosServices = axios.create({ baseURL: 'https://postgresql-server.onrender.com' });
 
 export default axiosServices;
